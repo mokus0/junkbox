b2a208d3017f3872eb16e570275b9d69f6218076
James Cook <mokus@deepbondi.net>
2008-05-15 19:26:11 +0000

    idioms: new module "Data.ToString"

---
+++ Idioms, &c./Data/ToString.hs
@@ -0,0 +1,20 @@
+{-
+ -      ``Data/ToString''
+ -      (c) 2008 Cook, J. MR  SSD, Inc.
+ -}
+{-# LANGUAGE
+        MultiParamTypeClasses,
+        FlexibleInstances,
+        UndecidableInstances,
+        OverlappingInstances
+  #-}
+
+module Data.ToString where
+
+import Data.StateRef
+
+class Monad m => ToString m a where
+        toString :: a -> m String
+
+instance (Show a, StateRef sr m a) => ToString m sr
+        where toString = (>>= (return . show)) . readRef
