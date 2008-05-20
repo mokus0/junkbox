676aa2f327896a049235d6434764e1a2a687c916
James Cook <mokus@deepbondi.net>
2008-05-20 17:35:29 +0000

    idioms: move maxBy, minimaBy to ReOrd, and add minBy, maximaBy, minima, and maxima

--- Idioms, &c./Util/Misc.hs
+++ Idioms, &c./Util/Misc.hs
@@ -19,11 +19,6 @@ import Control.Monad.Loops
 count :: (a -> Bool) -> [a] -> Int
 count f = length . filter f
 
-maxBy :: (a -> a -> Ordering) -> a -> a -> a
-maxBy (*) x y = case x * y of
-        LT      -> y
-        _       -> x
-
 limit :: Integral b => [a] -> b -> [a] -> [a]
 limit _   _      []    = []
 limit end (n+1) (x:xs) = x : limit end n xs
@@ -37,16 +32,6 @@ tryReadTChan c = fmap Just (readTChan c) `orElse` return Nothing
 
 readAllTChan :: TChan a -> STM [a]
 readAllTChan c = unfoldM (tryReadTChan c)
-
-
-minimaBy :: (a -> a -> Ordering) -> [a] -> [a]
-minimaBy cmp list = minimaBy' [] list
-        where   minimaBy' ms []           = ms
-                minimaBy' [] (x:xs)       = minimaBy' [x] xs
-                minimaBy' ms@(m:_) (x:xs) = case m `cmp` x of
-                        LT -> minimaBy' ms      xs
-                        EQ -> minimaBy' (x:ms)  xs
-                        GT -> minimaBy' [x]     xs
 
 mean :: Fractional a => [a] -> a
 mean xs = sum xs / fromIntegral (length xs)
--- Idioms, &c./Util/ReOrd.hs
+++ Idioms, &c./Util/ReOrd.hs
@@ -25,3 +25,42 @@ instance Eq (ReOrd a) where
 
 instance Ord (ReOrd a) where
         compare a b = cmp a (item a) (item b)
+
+-- some other alternative implementations of order-related functions,
+-- not necessarily using the ReOrd type
+
+minBy :: (a -> a -> Ordering) -> a -> a -> a
+minBy (*) x y = case x * y of
+        GT      -> y
+        _       -> x
+
+maxBy :: (a -> a -> Ordering) -> a -> a -> a
+maxBy (*) x y = case x * y of
+        LT      -> y
+        _       -> x
+
+
+-- minimaBy, maximaBy :: like minimumBy, maximumBy, but return _all_
+-- matches.  similarly 'minima', 'maxima'
+minimaBy :: (a -> a -> Ordering) -> [a] -> [a]
+minimaBy cmp list = minimaBy' [] list
+        where   minimaBy' ms []           = ms
+                minimaBy' [] (x:xs)       = minimaBy' [x] xs
+                minimaBy' ms@(m:_) (x:xs) = case m `cmp` x of
+                        LT -> minimaBy' ms      xs
+                        EQ -> minimaBy' (x:ms)  xs
+                        GT -> minimaBy' [x]     xs
+
+maximaBy :: (a -> a -> Ordering) -> [a] -> [a]
+maximaBy cmp list = maximaBy' [] list
+        where   maximaBy' ms []           = ms
+                maximaBy' [] (x:xs)       = maximaBy' [x] xs
+                maximaBy' ms@(m:_) (x:xs) = case m `cmp` x of
+                        LT -> maximaBy' [x]     xs
+                        EQ -> maximaBy' (x:ms)  xs
+                        GT -> maximaBy' ms      xs
+
+
+minima, maxima :: (Ord a) => [a] -> [a]
+minima = minimaBy compare
+maxima = maximaBy compare
