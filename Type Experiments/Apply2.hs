d93632520fe2a79f819b4df2830a533d17985bf3
James Cook <mokus@deepbondi.net>
2008-06-25 19:38:31 +0000

    Apply2: improved Relation/Set instances

--- Type Experiments/Apply2.hs
+++ Type Experiments/Apply2.hs
@@ -40,10 +40,7 @@ instance (Apply b c, Apply a (Result b c)) => Apply (Compose a b) c where
         type Result (Compose a b) c = Result a (Result b c)
         Compose f g $ x = f $ (g $ x)
 
-class ( Apply f x
-      , Apply (Inverse f) (Result f x)
-      , Result (Inverse f) (Result f x) ~ x
-      ) => Invertible f x where
+class Invertible f where
         type Inverse f :: *
         invert :: f -> Inverse f
 
@@ -51,33 +48,31 @@ instance (a ~ c) => Apply (a -> b) c where
         type Result (a -> b) c = b
         f $ x = f x
 
--- can't use this in conjunction with the next instance (and this instance can't be made "invertible"):
---instance (a ~ c) => Apply (Relation a b) c where
---        type Result (Relation a b) c = Set b
---        (Relation r) $ x = Set (r x)
+-- can't use these 2 instances together (c and Set c overlap):
+instance (a ~ c) => Apply (Relation a b) c where
+        type Result (Relation a b) c = Set b
+        (Relation r) $ x = Set (r x)
 
-class Existential c where
-        exists :: (c -> Bool) -> Bool
+--class Existential c where
+--        exists :: (c -> Bool) -> Bool
+--
+--instance (a ~ c, Existential c) => Apply (Relation a b) (Set c) where
+--        type Result (Relation a b) (Set c) = Set b
+--        (Relation r) $ (Set s) = Set (\y -> exists (\x -> s x && r x y))
 
-instance (a ~ c, Existential c) => Apply (Relation a b) (Set c) where
-        type Result (Relation a b) (Set c) = Set b
-        (Relation r) $ (Set s) = Set (\y -> exists (\x -> s x && r x y))
-
--- this seems right, but it panics ghc (6.9.20080602):
---instance (a ~ c, Existential a, Existential b) => Invertible (Relation a b) (Set c) where
---        type Inverse (Relation a b) = Relation b a
---        invert (Relation r) = Relation (flip r)
+instance Invertible (Relation a b) where
+        type Inverse (Relation a b) = Relation b a
+        invert (Relation r) = Relation (flip r)
 
 -- problematic: this should be able to have a simple polymorphic return type (Monad m => m v)
 instance (Ord k, k ~ k') => Apply (M.Map k v) k' where
         type Result (M.Map k v) k' = Maybe v
         f $ x = M.lookup x f
 
--- in general, partial functions are problematic at the type level...
---instance (k ~ k') => Invertible (M.Map k v) k' where
---        type Inverse (M.Map k v) = M.Map v k
---        invert m = M.fromList (map swap (M.toList m))
---                where swap = uncurry (flip (,))
+instance (Ord v) => Invertible (M.Map k v) where
+        type Inverse (M.Map k v) = M.Map v k
+        invert m = M.fromList (map swap (M.toList m))
+                where swap = uncurry (flip (,))
 
 newtype Map f = Map f
 map f x = Map f $ x
@@ -85,6 +80,10 @@ map f x = Map f $ x
 instance (Functor f, Apply a b) => Apply (Map a) (f b) where
         type Result (Map a) (f b) = f (Result a b)
         Map f $ xs = fmap (f $) xs
+
+instance Invertible f => Invertible (Map f) where
+        type Inverse (Map f) = Map (Inverse f)
+        invert (Map f) = Map (invert f)
 
 newtype Bind x = Bind x
 x >>= f         = Bind f $ x
