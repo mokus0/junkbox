13ff7c4f80cf052009af66b4d85cb2b0673cb4f8
James Cook <mokus@deepbondi.net>
2008-06-25 19:43:09 +0000

    Type Experiments/Set : fleshed out a bit.  typechecks, but depends on a typeclass implementing 'existential quantification' over function types

--- Type Experiments/Set.hs
+++ Type Experiments/Set.hs
@@ -2,10 +2,20 @@
  -      ``Set''
  -      (c) 2008 Cook, J. MR  SSD, Inc.
  -}
+{-# LANGUAGE 
+        FlexibleContexts
+  #-}
 
 module Set where
 
 type Set a = a -> Bool
+type Relation a b = a -> b -> Bool
 
-join :: Set (Set a) -> Set a
-join s = \e -> ???
+contains :: Relation (Set a) a
+set `contains` elem = set elem
+
+class Existential c where
+        exists :: (c -> Bool) -> Bool
+
+join :: Existential (Set a) => Set (Set a) -> Set a
+join s = \e -> exists (\s -> s `contains` e)
