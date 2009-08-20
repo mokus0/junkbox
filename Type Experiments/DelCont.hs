e49676564ee39e9796bd27aa2b815f57d4828d02
James Cook <mokus@deepbondi.net>
2009-08-20 15:52:20 +0000

    Refactored DelCont.hs out of CPS (was passing a real "Sctx" continuation, which is not needed)

--- Type Experiments/DelCont.hs
+++ Type Experiments/DelCont.hs
@@ -4,39 +4,30 @@ module DelCont where
 -- (requires NoImplicitPrelude in code using them)
 import Prelude hiding (return, (>>=), (>>), fail)
 
-newtype CC ans a b t =
-    CC { runCC :: Ctn ans t a
-               -> Sctx ans b
-               -> ans
-        -- could also add a dummy ()-> on last ans, to make the whole
-        -- type: Fun () a t b ans
-        -- AKA: ()/a -> t/b
-       }
+newtype CC a b t =
+    CC { runCC :: (t -> b) -> a }
 
-type Sctx ans val = val -> ans
-type Ctn ans s a = Sctx ans a -> Sctx ans s
--- Danvy & Filinski's s/a -> t/b, I think
--- (might have switched a and b, I never could keep those straight)
-type Fun ans s a t b = Ctn ans t a -> Ctn ans s b
+eval x = runCC x id
+reset = resetTo id
+resetTo f x = CC (\k -> k (runCC x f))
 
-eval x = runCC x id id
+return x = CC (\k -> k x)
+shift  f = CC $ \k -> runCC (f (resetTo k)) id
+shift' f = CC $ \k -> runCC (f (\x -> CC (\sk -> sk (k x)))) id
+abort  x = CC $ \k -> runCC x id
 
-return x = CC $ \k gam -> k gam x
-shift  f = CC $ \k gam -> runCC (f (resetTo k)) id gam
-shift' f = CC $ \k gam -> runCC (f (\x -> CC (\k' gam' -> k (k' gam') x))) id gam
-abort  x = CC $ \k gam -> runCC x id gam
-
-reset x = resetTo id x
-resetTo f x = CC $ \k -> runCC x f . k
-
-lift :: (s -> CC ans a b t) -> Fun ans s a t b
-lift f k = ctn f k -- . k
-
-ctn f k gam val = runCC (f val) k gam
-
-infixl 1 >>=, >>
-(>>=) :: CC ans b c s -> (s -> CC ans a b t) -> CC ans a c t
-x >>= f = CC $ \k gam -> runCC x (lift f k) gam
+infixl 1 >>=, >>, >=>
+(>>=) :: CC a b s -> (s -> CC b c t) -> CC a c t
+x >>= f = CC $ \k -> runCC x (lift f k)
 x >> y = x >>= const y
+
+lift :: (s -> CC b c t) -> (t -> c) -> s -> b
+lift f k val = runCC (f val) k
+
+-- ok, this is how I think the order of a and b in s/a -> t/b is chosen:
+-- (at least, it sure would make a lot of sense)
+type Fun s a t b = s -> CC a b t
+(>=>) :: Fun s a t b -> Fun t b u c -> Fun s a u c
+f >=> g = \x -> f x >>= g
 
 fail = abort . return
