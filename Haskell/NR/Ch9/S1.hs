6020cf5d04c7deef47122a00bcff0bb4a5291c6b
James Cook <mokus@deepbondi.net>
2010-04-28 20:52:57 +0000

    Abstracted the root-finding system (experimental)

--- Haskell/NR/Ch9/S1.hs
+++ Haskell/NR/Ch9/S1.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, ScopedTypeVariables, FlexibleContexts #-}
 module NR.Ch9.S1 where
 
 -- |Given a function and an initial guessed range x1 to x2, this function
@@ -32,45 +33,74 @@ zbrak f x1 x2 n =
         xs = x1 : [x1 + dx * fromIntegral i | i <- [1..n]]
         xys = map (\x -> (x, f x)) xs
 
--- |Using bisection, return a root of a function known to lie between x1 and x2.
--- The root will be refined till its accuracy is +-xacc.
-rtbis f x1 x2 xacc = convergeToEps "rtbis" xacc (bisect f x1 x2)
+class RootFinder r a b where
+    initRootFinder :: (a -> b) -> a -> a -> r a b
+    stepRootFinder :: (a -> b) -> r a b -> r a b
+    estimateRoot  :: r a b -> a
+    estimateError :: r a b -> a
+    
+    converged :: (Num a, Ord a) => a -> r a b -> Bool
+    converged xacc r = abs (estimateError r) <= abs xacc
+    
+    defaultNSteps :: r a b -> Int
+    defaultNSteps _ = 250
+    stepsTaken    :: r a b -> Maybe Int
+    stepsTaken _ = Nothing
 
-converge eps = convergeToEps "converge" eps
-convergeToEps loc eps = fst . convergeTo loc 100 (\(x,dx) -> abs dx <= eps)
-
--- |Given a (non-empty) list of improving estimates for a value, find either
--- the final  estimate or the first one satisfying a predicate.  Limit
--- the search to i given number 'n' of tries and fail if that number is 
--- exceeded, using the given string as a description of the location of failure
--- (typically a function name).
-convergeTo loc _ _ [] = error (loc ++ ": empty list")
-convergeTo loc n p xs = go n xs
+traceRoot f a b xacc = go nSteps start (stepRootFinder f start)
     where
-        go n [x] = x
-        go n (x:xs)
-            | n <= 0    = error (loc ++ ": too many steps")
-            | p x       = x
-            | otherwise = go (n+1) xs
-
--- |Bisect an interval in search of the root, returning at each step a guess at
--- the location of the root and the width of the current interval.  In the event
--- of discovering an exact zero, returns the corresponding abscissa as the last
--- element of the list.
---
--- Each element of the returned list is a pair (x,dx) where f x <= 0 and f (x+dx) >= 0.
-bisect f x1 x2
-    | f1 < 0    = go (x2-x1) x1 (f x1)
-    | otherwise = go (x1-x2) x2 (f x2)
-    where
-        f1 = f x1
-        f2 = f x2
+        nSteps = defaultNSteps start
+        start = initRootFinder f a b
         
-        go dx x fx = case fMid `compare` 0 of
-            LT ->  (x, dx) : go dx2 xMid fMid
-            EQ ->  [(xMid, 0)]
-            GT ->  (x, dx) : go dx2 x fx
+        -- lookahead 1; if tracing with no convergence test, apply a
+        -- naive test to bail out if the root stops changing.  This is
+        -- provided because that's not always the same as convergence to 0,
+        -- and the main purpose of this function is to watch what actually
+        -- happens inside the root finder.
+        go n x next
+            | maybe (x==next) (flip converged x) xacc = [x]
+            | n <= 0            = []
+            | otherwise         = x : go (n-1) next (stepRootFinder f next)
+
+findRoot f a b xacc = go nSteps start
+    where
+        nSteps = defaultNSteps start
+        start = initRootFinder f a b
+        
+        go n x
+            | converged xacc x  = x
+            | n <= 0    = error "findRoot: too many iterations"
+            | otherwise         = go (n-1) (stepRootFinder f x)
+
+-- |Bisect an interval in search of a root.  At all times, @f (estimateRoot _)@
+-- is less than or equal to 0 and @f (estimateRoot _ + estimateError _)@ is 
+-- greater than or equal to 0.
+data Bisect a b = Bisect { bisX :: !a, bisF :: !b , bisDX :: !a }
+    deriving (Eq, Ord, Show)
+
+instance (Fractional a, Ord b, Num b) => RootFinder Bisect a b where
+    initRootFinder f x1 x2
+        | f1 < 0    = Bisect x1 f1 (x2-x1)
+        | otherwise = Bisect x2 f2 (x1-x2)
+        where f1 = f x1; f2 = f x2
+    
+    stepRootFinder f orig@(Bisect x fx dx) = case fMid `compare` 0 of
+            LT ->  Bisect xMid fMid dx2
+            EQ ->  orig
+            GT ->  Bisect x fx dx2 
             where
                 dx2 = dx * 0.5
                 xMid = x + dx2
                 fMid = f xMid
+    
+    estimateRoot  (Bisect x _  _) = x
+    
+    estimateError (Bisect _ 0  _) = 0
+    estimateError (Bisect _ _ dx) = dx
+
+-- |Using bisection, return a root of a function known to lie between x1 and x2.
+-- The root will be refined till its accuracy is +-xacc.
+rtbis f x1 x2 xacc = 
+    (estimateRoot :: (Fractional a, Ord b, Num b) => Bisect a b -> a)
+    (findRoot f x1 x2 xacc)
+
--- Haskell/NR/Ch9/S2.hs
+++ Haskell/NR/Ch9/S2.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 module NR.Ch9.S2 where
 
 import NR.Ch9.S1
@@ -6,36 +7,49 @@ import Data.Ord
 
 -- | Using the false-position method, return a root of a function known
 -- to lie between x1 and x2.  The root is refined until its accuracy is += xacc.
-rtflsp f x1 x2 xacc = convergeToEps "rtflsp" xacc (falsePosition f x1 x2)
-
+rtflsp f x1 x2 xacc = 
+    (estimateRoot :: (Fractional a, Ord a) => FalsePosition a a -> a)
+    (findRoot f x1 x2 xacc)
+    
 -- |Iteratively refine a bracketing interval [x1, x2] of a root of f
 -- until total convergence (which may or may not ever be achieved) using 
 -- the false-position method.
--- 
--- Each element of the returned list is a pair (x,dx) where
--- x is a new estimate of a root of f x and dx is the amount this
--- estimate differs from the last.
-falsePosition f x1 x2
-    | f1 <= 0 && f2 >= 0    = go x1 f1 x2 f2
-    | f2 <= 0 && f1 >= 0    = go x2 f2 x1 f1
-    | otherwise             = error "falsePosition: given interval does not bracket a root"
+data FalsePosition a b = FalsePosition
+    { fpRoot :: !a
+    , fpDX   :: !a
+    , fpXL   :: !a
+    , fpFL   :: !a
+    , fpXH   :: !a
+    , fpFH   :: !a
+    } deriving (Eq, Show)
 
-    where
-        f1 = f x1
-        f2 = f x2
-        
-        go xl fl xh fh = case compare fNew 0 of
-            LT -> (xNew, xl-xNew) : go xNew fNew  xh   fh
-            EQ -> [(xNew, 0)]
-            GT -> (xNew, xh-xNew) : go xl   fl    xNew fNew
-            where
-                dx = xh - xl
-                xNew = xl + dx * fl / (fl - fh)
-                fNew = f xNew
+instance (Fractional a, Ord a) => RootFinder FalsePosition a a where
+    initRootFinder f x1 x2
+        -- step once to compute first estimate
+        |  f1 <= 0 && f2 >= 0
+        || f2 <= 0 && f1 >= 0   = stepRootFinder f $ FalsePosition 0 0 x2 f2 x1 f1
+        | otherwise             = error "FalsePosition: given interval does not bracket a root"
+        where
+            f1 = f x1
+            f2 = f x2
+    
+    stepRootFinder f orig@(FalsePosition _ _ xl fl xh fh) = case compare fNew 0 of
+        LT -> FalsePosition xNew (xl - xNew) xNew fNew  xh   fh
+        EQ -> orig
+        GT -> FalsePosition xNew (xh - xNew) xl   fl    xNew fNew
+        where
+            dx = xh - xl
+            xNew = xl + dx * fl / (fl - fh)
+            fNew = f xNew
+    
+    estimateRoot = fpRoot
+    estimateError = fpDX
 
 -- |Using the secant method, return the root of a function thought to lie between
 -- x1 and x2.  The root is refined until its accuracy is +-xacc.
-rtsec f x1 x2 xacc = convergeToEps "rtsec" xacc (secantMethod f x1 x2)
+rtsec f x1 x2 xacc = 
+    (estimateRoot :: (Fractional a, Ord a) => SecantMethod a a -> a)
+    (findRoot f x1 x2 xacc)
 
 -- |Iteratively refine 2 estimates x1, x2 of a root of f until total 
 -- convergence (which may or may not ever be achieved) using the
@@ -44,50 +58,82 @@ rtsec f x1 x2 xacc = convergeToEps "rtsec" xacc (secantMethod f x1 x2)
 -- Each element of the returned list is a pair (x,dx) where
 -- x is a new estimate of a root of f x and dx is the amount this
 -- estimate differs from the last.
-secantMethod f x1 x2 
-    | abs f1 < abs f2       = go x2 f2 x1 f1
-    | otherwise             = go x1 f1 x2 f2
-    where
-        f1 = f x1
-        f2 = f x2
-        
-        go xl fl rts fRts
-            | fNew == 0 = [(xNew, 0)]
-            | otherwise = (rts, dx) : go rts fRts xNew fNew
-            where
-                dx = (xl - rts) * fRts / (fRts - fl)
-                xNew = rts + dx
-                fNew = f xNew
 
-zriddr f x1 x2 xacc = convergeToEps "zriddr" xacc (riddersMethod f x1 x2)
+data SecantMethod a b
+    = ConvergedSecantMethod !a
+    | SecantMethod
+        { secDX
+        , secXL   :: !a
+        , secFL   :: !b
+        , secRTS  :: !a
+        , secFRTS :: !b
+        } deriving (Eq, Show)
 
-riddersMethod f x1 x2 
-    |  f1 < 0 && f2 < 0
-    || f2 > 0 && f1 > 0 = error "riddersMethod: interval does not bracket a root"
-    | otherwise         = tail (go x1 f1 x2 f2 undefined)
-    where
-        f1 = f x1
-        f2 = f x2
-        
-        -- TODO: currently, ans is undefined on first pass, so cannot check xNew==ans.  shift endpoints of loop to make ans well-defined at all times.
-        go xl fl xh fh ans 
-            | xl == xh  = [(ans, 0)]
-            | fNew == 0 = [(xNew, 0)]
-            | otherwise = (ans, xNew - ans) : rebrac xl fl xm fm xh fh xNew fNew
+instance (Fractional a, Ord a) => RootFinder SecantMethod a a where
+    initRootFinder f x1 x2
+        | abs f1 < abs f2       = stepRootFinder f $ SecantMethod 0 x2 f2 x1 f1
+        | otherwise             = stepRootFinder f $ SecantMethod 0 x1 f1 x2 f2
+        where f1 = f x1; f2 = f x2
+    
+    stepRootFinder f orig@ConvergedSecantMethod{} = orig
+    stepRootFinder f (SecantMethod _ xl fl rts fRts)
+        | fNew == 0 = ConvergedSecantMethod xNew
+        | otherwise = SecantMethod dx rts fRts xNew fNew
+        where
+            dx = (xl - rts) * fRts / (fRts - fl)
+            xNew = rts + dx
+            fNew = f xNew
+    
+    estimateRoot (ConvergedSecantMethod x)  = x
+    estimateRoot SecantMethod{secXL = x}    = x
+    
+    estimateError ConvergedSecantMethod{}   = 0
+    estimateError SecantMethod{secDX = dx}  = dx
+
+zriddr f x1 x2 xacc =
+    (estimateRoot :: (Floating a, Ord a) => RiddersMethod a a -> a)
+    (findRoot f x1 x2 xacc)
+
+data RiddersMethod a b
+    = ConvergedRidders !a
+    | RiddersMethod
+        { ridXL     :: !a
+        , ridFL     :: !b
+        , ridXH     :: !a
+        , ridFH     :: !b
+        } deriving (Eq, Show)
+
+instance (Floating a, Ord a) => RootFinder RiddersMethod a a where
+    initRootFinder f x1 x2
+        |  f1 < 0 && f2 < 0
+        || f2 > 0 && f1 > 0 = error "riddersMethod: interval does not bracket a root"
+        | otherwise         = RiddersMethod x1 f1 x2 f2
+        where
+            f1 = f x1
+            f2 = f x2
+    stepRootFinder f orig@ConvergedRidders{} = orig
+    stepRootFinder f (RiddersMethod xl fl xh fh)
+            | signNEQ fm fNew   = finish xNew fNew xm fm
+            | signNEQ fl fNew   = finish xNew fNew xl fl
+            | signNEQ fh fNew   = finish xNew fNew xh fh
+            | otherwise         = error "RiddersMethod: encountered singularity"
             where
                 xm = 0.5 * (xl + xh)
                 fm = f xm
                 s = sqrt (fm*fm - fl*fh)
                 xNew = xm + (xm-xl)*((if fl >= fh then id else negate) fm / s)
                 fNew = f xNew
-
-        rebrac xl fl xm fm xh fh xNew fNew
-            | foo fm fNew   = go xm   fm   xNew fNew xNew
-            | foo fl fNew   = go xl   fl   xNew fNew xNew
-            | foo fh fNew   = go xNew fNew xh   fh   xNew
-            | otherwise                 = error "zriddr: encountered singularity"
-        foo a b = a /= 0 && signum b /= signum a
-
-minBy cmp x y = case cmp x y of
-    LT -> x
-    _  -> y
+                
+                signNEQ a b = a /= 0 && signum b /= signum a
+                
+                finish xl fl xh fh
+                    | xl == xh  = ConvergedRidders xl
+                    | fl == 0   = ConvergedRidders xl
+                    | fh == 0   = ConvergedRidders xh
+                    | otherwise = RiddersMethod xl fl xh fh
+    
+    estimateRoot (ConvergedRidders x)       = x
+    estimateRoot RiddersMethod{ridXL = x}  = x
+    
+    estimateError ConvergedRidders{}        = 0
+    estimateError RiddersMethod{ridXL = xl, ridXH = xh} = xl - xh
