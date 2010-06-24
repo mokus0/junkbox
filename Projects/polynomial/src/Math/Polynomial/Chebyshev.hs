c038bf4845979ca33dfc34853e771c51d6545e84
James Cook <mokus@deepbondi.net>
2010-06-24 16:16:22 +0000

    Implemented chebyshev series fitting and evaluation

--- Projects/polynomial/src/Math/Polynomial/Chebyshev.hs
+++ Projects/polynomial/src/Math/Polynomial/Chebyshev.hs
@@ -36,14 +36,7 @@ evalT n x = evalTs x !! n
 
 -- |Evaluate all the Chebyshev polynomials of the first kind at a point X.
 evalTs :: Num a => a -> [a]
-evalTs x = ts
-    where
-       ts = 1 : x : 
-            [ 2 * x * t_n - t_nm1
-            | n     <- iterate (1+) 1
-            | t_n   <- tail ts
-            | t_nm1 <- ts
-            ]
+evalTs = fst . evalTsUs
 
 -- |Evaluate the n'th Chebyshev polynomial of the second kind at a point X.  
 -- Both more efficient and more numerically stable than computing the 
@@ -53,27 +46,40 @@ evalU n x = evalUs x !! n
 
 -- |Evaluate all the Chebyshev polynomials of the second kind at a point X.
 evalUs :: Num a => a -> [a]
-evalUs x = us
-    where
-       us = 0 : 1 : 
-            [ 2*x*u_n - u_nm1
-            | n     <- iterate (1+) 1
-            | u_n   <- tail us
-            | u_nm1 <- us
-            ]
+evalUs = snd . evalTsUs
 
+-- |Evaluate the n'th Chebyshev polynomials of both kinds at a point X.
 evalTU :: Num a => Int -> a -> (a,a)
 evalTU n x = (ts!!n, us!!n)
     where (ts,us) = evalTsUs x
 
+-- |Evaluate all the Chebyshev polynomials of the both kinds at a point X.
 evalTsUs :: Num a => a -> ([a], [a])
 evalTsUs x = (ts, us)
     where
         ts = 1 : [x * t_n - (1-x*x)*u_n  | t_n <- ts | u_n <- us]
         us = 0 : [x * u_n + t_n          | t_n <- ts | u_n <- us]
 
+-- |Compute the roots of the n'th Chebyshev polynomial of the first kind.
 tRoots :: Floating a => Int -> [a]
 tRoots   n = [cos (pi / fromIntegral n * (fromIntegral k + 0.5)) | k <- [0..n-1]]
 
+-- |Compute the extreme points of the n'th Chebyshev polynomial of the first kind.
 tExtrema :: Floating a => Int -> [a]
 tExtrema n = [cos (pi / fromIntegral n *  fromIntegral k       ) | k <- [0..n]]
+
+-- |Estimate the first N coefficients of the Chebyshev series expansion of a
+-- given function.
+chebyshevFit :: Floating a => Int -> (a -> a) -> [a]
+chebyshevFit n f = 
+    [ 2 / fromIntegral n * sum 
+        [ f x * evalT j x
+        | x <- tRoots n
+        ]
+    | j <- [0..n-1]
+    ]
+
+-- |Evaluate a Chebyshev series expansion with a finite number of terms.
+evalChebyshevSeries :: Fractional a => [a] -> a -> a
+evalChebyshevSeries []      = \x -> 0
+evalChebyshevSeries (c0:cs) = \x -> sum (zipWith (*) (0.5 * c0 : cs) (evalTs x))
