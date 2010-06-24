045c3a2c2f4e5b102205ab106a6f710a27c44862
James Cook <mokus@deepbondi.net>
2010-06-24 15:42:17 +0000

    Generalized Chebyshev polynomials to Num instead of Fractional contexts, since all the coefficients are integers.

--- Projects/polynomial/src/Math/Polynomial/Chebyshev.hs
+++ Projects/polynomial/src/Math/Polynomial/Chebyshev.hs
@@ -3,8 +3,8 @@ module Math.Polynomial.Chebyshev where
 
 import Math.Polynomial
 
--- |The Chebyshev polynomials of the first kind with 'Rational' coefficients.
-ts :: [Poly Rational]
+-- |The Chebyshev polynomials of the first kind with 'Integer' coefficients.
+ts :: [Poly Integer]
 ts = poly LE [1] : 
     [ addPoly (poly LE [0, 1]    `multPoly` t_n)
               (poly LE [-1,0,1] `multPoly` u_n)
@@ -12,8 +12,8 @@ ts = poly LE [1] :
     | u_n <- us
     ]
 
--- The Chebyshev polynomials of the second kind with 'Rational' coefficients.
-us :: [Poly Rational]
+-- The Chebyshev polynomials of the second kind with 'Integer' coefficients.
+us :: [Poly Integer]
 us = poly LE [0] : 
     [ addPoly t_n (multPoly u_n (poly LE [0,1]))
     | t_n <- ts
@@ -21,21 +21,21 @@ us = poly LE [0] :
     ]
 
 -- |Compute the coefficients of the n'th Chebyshev polynomial of the first kind.
-t :: Fractional a => Int -> Poly a
-t n = poly LE . map fromRational . polyCoeffs LE $ ts !! n
+t :: Num a => Int -> Poly a
+t n = poly LE . map fromInteger . polyCoeffs LE $ ts !! n
 
 -- |Compute the coefficients of the n'th Chebyshev polynomial of the second kind.
-u :: Fractional a => Int -> Poly a
-u n = poly LE . map fromRational . polyCoeffs LE $ us !! n
+u :: Num a => Int -> Poly a
+u n = poly LE . map fromInteger . polyCoeffs LE $ us !! n
 
 -- |Evaluate the n'th Chebyshev polynomial of the first kind at a point X.  
 -- Both more efficient and more numerically stable than computing the 
 -- coefficients and evaluating the polynomial.
-evalT :: Fractional a => Int -> a -> a
+evalT :: Num a => Int -> a -> a
 evalT n x = evalTs x !! n
 
 -- |Evaluate all the Chebyshev polynomials of the first kind at a point X.
-evalTs :: Fractional a => a -> [a]
+evalTs :: Num a => a -> [a]
 evalTs x = ts
     where
        ts = 1 : x : 
@@ -48,11 +48,11 @@ evalTs x = ts
 -- |Evaluate the n'th Chebyshev polynomial of the second kind at a point X.  
 -- Both more efficient and more numerically stable than computing the 
 -- coefficients and evaluating the polynomial.
-evalU :: Fractional a => Int -> a -> a
+evalU :: Num a => Int -> a -> a
 evalU n x = evalUs x !! n
 
 -- |Evaluate all the Chebyshev polynomials of the second kind at a point X.
-evalUs :: Fractional a => a -> [a]
+evalUs :: Num a => a -> [a]
 evalUs x = us
     where
        us = 0 : 1 : 
@@ -61,6 +61,16 @@ evalUs x = us
             | u_n   <- tail us
             | u_nm1 <- us
             ]
+
+evalTU :: Num a => Int -> a -> (a,a)
+evalTU n x = (ts!!n, us!!n)
+    where (ts,us) = evalTsUs x
+
+evalTsUs :: Num a => a -> ([a], [a])
+evalTsUs x = (ts, us)
+    where
+        ts = 1 : [x * t_n - (1-x*x)*u_n  | t_n <- ts | u_n <- us]
+        us = 0 : [x * u_n + t_n          | t_n <- ts | u_n <- us]
 
 tRoots :: Floating a => Int -> [a]
 tRoots   n = [cos (pi / fromIntegral n * (fromIntegral k + 0.5)) | k <- [0..n-1]]
