20bed0372115332ad6c4829ab595e2f644bd0786
James Cook <mokus@deepbondi.net>
2010-07-13 21:45:16 +0000

    silly experiment to use TH to "automagically" reify code, eg, for a mobile computing framework or something

---
+++ Haskell/TypeExperiments/CerealBawksz.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE TemplateHaskell, MagicHash #-}
+module TypeExperiments.CerealBawksz where
+
+import Data.Ratio
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+import Language.Haskell.TH.Lift
+
+$( fmap concat $ mapM deriveLift
+    [ ''Exp
+    , ''Type
+    , ''Range
+    , ''Stmt
+    , ''Match
+    , ''Dec
+    , ''Pat
+    , ''Lit
+    , ''Kind
+    , ''Pred
+    , ''TyVarBndr
+    , ''Body
+    , ''FamFlavour
+    , ''Pragma
+    , ''Foreign
+    , ''FunDep
+    , ''Con
+    , ''Clause
+    , ''Guard
+    , ''InlineSpec
+    , ''Safety
+    , ''Callconv
+    , ''Strict
+    ])
+
+instance (Lift a, Integral a) => Lift (Ratio a) where
+    lift x = [| p % q |]
+        where p = numerator x; q = denominator x
+    
+
+-- All that to say this:
+
+-- | Take a TH 'ExpQ' and transform it to include
+-- its own source.  For example:
+-- 
+-- > foo :: Num a => a -> a
+-- > (fooCode, foo) = $(code [| \x -> x^2 - 1 |])
+code expr = do
+    expr' <- expr
+    [| (expr', $(expr)) |]
