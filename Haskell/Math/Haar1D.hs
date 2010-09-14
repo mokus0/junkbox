cdebaa4ef749e55d619299abeaff11f88752ee93
James Cook <mokus@deepbondi.net>
2010-09-14 15:39:57 +0000

    Implemented inverse of 'haar' function in Haar1D.hs

--- Haskell/Math/Haar1D.hs
+++ Haskell/Math/Haar1D.hs
@@ -1,7 +1,10 @@
 -- translation of SML code from wikipedia, mostly just an exercise in reading ML
+-- Turns out this is a pretty goofy implementation.  It reverses every other level,
+-- which decodes just fine as long as you don't try to truncate the encoded form
+-- by an odd number of levels.
 module Math.Haar1D where
 
-haar [] = error "haar: empty list"
+haar [] = []
 haar l = aux l [] []
     where
         aux [s] [] d = s : d
@@ -9,3 +12,10 @@ haar l = aux l [] []
         aux (h1:h2:t) s d = aux t ((h1 + h2) : s) ((h1 - h2) : d)
         aux _ _ _ = error "haar: list length was not a power of 2"
 
+invHaar [] = []
+invHaar (s:xs) = extend [s] xs []
+    where
+        extend ss [] [] = ss
+        extend [] xs zs = extend zs xs []
+        extend (s:ss) (d:ds) zs = extend ss ds (0.5 * (s + d) : 0.5 * (s - d) : zs)
+        extend _ _ _ = error "invHaar: list length was not a power of 2"
