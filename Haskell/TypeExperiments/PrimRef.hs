9de02404f59b0d04c7d9016c79c735332f0d668a
james.cook <james.cook@usma.edu>
2011-02-25 10:16:29 -0500

    Added STRef support to PrimRef thing (alternate parameterization of reference type)

--- Haskell/TypeExperiments/PrimRef.hs
+++ Haskell/TypeExperiments/PrimRef.hs
@@ -4,24 +4,57 @@ module TypeExperiments.PrimRef
     , readRef
     , writeRef
     , modifyRef
+    
+    , refToSTRef
+    , refFromSTRef
+    
+    , STRef
+    , newSTRef
+    , readSTRef
+    , writeSTRef
+    , modifySTRef
     ) where
 
 import Control.Applicative
 import Control.Monad.Primitive
-import Control.Monad.ST
-import Data.STRef
+import qualified Control.Monad.ST as ST
+import Data.STRef (STRef)
+import qualified Data.STRef       as ST
 
-newtype Ref m a = Ref (STRef (PrimState m) a)
+newtype Ref m a = Ref (ST.STRef (PrimState m) a)
     deriving (Eq)
 
 newRef :: PrimMonad m => a -> m (Ref m a)
-newRef x = primToPrim (Ref <$> newSTRef x)
+newRef x = primToPrim (Ref <$> ST.newSTRef x)
 
 readRef :: PrimMonad m => Ref m a -> m a
-readRef (Ref r) = primToPrim (readSTRef r)
+readRef (Ref r) = readSTRef r
 
 writeRef :: PrimMonad m => Ref m a -> a -> m ()
-writeRef (Ref r) = primToPrim . writeSTRef r
+writeRef (Ref r) = writeSTRef r
 
 modifyRef :: PrimMonad m => Ref m a -> (a -> a) -> m ()
-modifyRef (Ref r) = primToPrim . modifySTRef r
+modifyRef (Ref r) = modifySTRef r
+
+
+refToSTRef :: Ref m a -> STRef (PrimState m) a
+refToSTRef (Ref r) = r
+
+refFromSTRef :: STRef (PrimState m) a -> Ref m a
+refFromSTRef = Ref
+
+
+-- similar case with less-constrained reference type (uses 'STRef' as-is,
+-- parameterized over state rather than monad, so different monads with same
+-- state-thread can share refs)
+newSTRef :: PrimMonad m => a -> m (STRef (PrimState m) a)
+newSTRef x = primToPrim (ST.newSTRef x)
+
+readSTRef :: PrimMonad m => STRef (PrimState m) a -> m a
+readSTRef r = primToPrim (ST.readSTRef r)
+
+writeSTRef :: PrimMonad m => STRef (PrimState m) a -> a -> m ()
+writeSTRef r x = primToPrim (ST.writeSTRef r x)
+
+modifySTRef :: PrimMonad m => STRef (PrimState m) a -> (a -> a) -> m ()
+modifySTRef r f = primToPrim (ST.modifySTRef r f)
