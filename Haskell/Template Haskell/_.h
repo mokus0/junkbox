be9d3b05fc2ec90586ee7866b463c8fa362df44a
James Cook <mokus@deepbondi.net>
2011-04-05 11:45:34 -0400

    Silly little CPP+TH hack to support _ as a wildcard expression

---
+++ Haskell/Template Haskell/_.h
@@ -0,0 +1,6 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+import Underscore
+
+#define undefined   [underscore|"undefined" at line __LINE__ in __FILE__|]
+#define _           [underscore|"_" at line __LINE__ in __FILE__|]
