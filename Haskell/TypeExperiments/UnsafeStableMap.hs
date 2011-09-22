c509ffd2719aa6b4d69e8cbc3ff45987f5a1f6f4
James Cook <mokus@deepbondi.net>
2011-09-22 14:13:07 -0400

    Slight simplification in TypeExperiments.UnsafeStableMap

--- Haskell/TypeExperiments/UnsafeStableMap.hs
+++ Haskell/TypeExperiments/UnsafeStableMap.hs
@@ -2,12 +2,13 @@ module TypeExperiments.UnsafeStableMap where
 
 import Prelude hiding (lookup)
 
+import Data.Functor.Identity
 import System.Mem.StableName
 import System.Mem.StableName.Map
 
 -- |A simple demonstration of the fact that the stable-maps library is
 -- not type-safe.  This function is behaviorally nearly identical to
--- @return . unsafeCoerce@.
+-- @return unsafeCoerce@.
 -- 
 -- The core of the problem is that sn1 and sn2 are equal (after type erasure)
 -- because they were constructed from the same closure ('undefined').
@@ -16,18 +17,17 @@ import System.Mem.StableName.Map
 -- (except maybe if type-classes are involved, because then the closures
 -- may not be the same at different types).  In particular, the value need
 -- not be \"bottom\".  'id' works just fine, for example.
-unsafeCoerceIO :: a -> IO b
-unsafeCoerceIO x = do
+unsafeCoerceIO :: IO (a -> b)
+unsafeCoerceIO = do
     sn1 <- makeStableName undefined
     sn2 <- makeStableName undefined
     
-    let m = singleton sn1 (Just x)
-        Just (Just y) = lookup sn2 m
-    
-    return y
-
+    return $ \x ->
+        let m = singleton sn1 (Identity x)
+            Just (Identity y) = lookup sn2 m
+         in y
 
 main :: IO ()
 main = do
-    f <- unsafeCoerceIO ()
-    f "what did you do to my argument stack?!"
+    unsafeCoerce <- unsafeCoerceIO
+    unsafeCoerce () "what did you do to my argument stack?!"
