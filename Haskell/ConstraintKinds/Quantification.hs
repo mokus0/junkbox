a8a422fca6f53724baf324cf9a25bfe8810c88d6
James Cook <mokus@deepbondi.net>
2011-10-18 09:21:49 -0400

    More fun with ConstraintKinds: quantification...
    ... over predicates of kind '* -> Constraint'

---
+++ Haskell/ConstraintKinds/Quantification.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE ConstraintKinds, TypeFamilies, UndecidableInstances, 
+        ScopedTypeVariables, TypeOperators #-}
+
+module ConstraintKinds.Quantification where
+
+import ConstraintKinds.Dict
+import ConstraintKinds.Typeable
+import ConstraintKinds.Pred
+import Data.Tagged
+import Data.Typeable
+
+class p (Witness p) => Exists p where
+    type Witness p
+
+class Forall p where
+    witness :: Dict (p a)
+
+instance TypeableCxt1 p => TypeableCxt (Exists p) where
+    typeOfCxt = Tagged (mkTyConApp con [untag (typeOfCxt1 :: Tagged (Dict (p ())) TypeRep)])
+        where con = mkTyCon3 "junkbox" "ConstraintKinds.Quantification" "Exists"
+
+instance TypeableCxt1 p => TypeableCxt (Forall p) where
+    typeOfCxt = Tagged (mkTyConApp con [untag (typeOfCxt1 :: Tagged (Dict (p ())) TypeRep)])
+        where con = mkTyCon3 "junkbox" "ConstraintKinds.Quantification" "Forall"
+
+instance Forall True where witness = Dict
+instance (Forall p, Forall q) => Forall (p :&: q) where
+    witness = strengthen (witness :: Dict (p a)) (witness :: Dict (q a))
+
+instance Exists True where type Witness True = ()
+instance Exists ((~) a) where type Witness ((~) a) = a
+instance Exists Show where type Witness Show = ()
+instance Exists Read where type Witness Read = ()
+instance Exists Eq   where type Witness Eq   = ()
+
+-- and so on...
