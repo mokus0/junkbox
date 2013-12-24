e4500cb9e33d3cfac632dd71ae1e43a18d1109b6
James Cook <mokus@deepbondi.net>
2013-12-24 10:24:04 -0800

    minor simplification in BiQuad

--- Haskell/Math/BiQuad.hs
+++ Haskell/Math/BiQuad.hs
@@ -19,24 +19,21 @@ transfer BiQuad{..} z
     where zInv = recip z
 
 poles BiQuad{..} = (2 * a2 / d, 0.5 * d)
-    where
-        d = negate a1 - sqrt (a1*a1 - 4*a2)
-            --else negate a1 + sqrt (a1*a1 - 4*a2)
+    where d = negate a1 - sqrt (a1*a1 - 4*a2)
 
-directForm1 BiQuad{..} !x_n = do
-    (x_nm1, x_nm2, y_nm1, y_nm2) <- get
-    let !y_n =              a1 * y_nm1 + a2 * y_nm2
-             + b0 * x_n   + b1 * x_nm1 + b2 * x_nm2
-    put (x_n, x_nm1, y_n, y_nm1)
+directForm1 BiQuad{..} !x0 = do
+    (x1, x2, y1, y2) <- get
+    let !y0 =              a1 * y1 + a2 * y2
+             + b0 * x0   + b1 * x1 + b2 * x2
+    put (x0, x1, y0, y1)
     
-    return y_n
+    return y0
 
-directForm2 BiQuad{..} !x_n = do
-    (w_nm1, w_nm2) <- get
-    let !w_n =      x_n - a1 * w_nm1 - a2 * w_nm2
-        !y_n = b0 * w_n + b1 * w_nm1 + b2 * w_nm2
-    put (w_n, w_nm1)
+directForm2 BiQuad{..} !x = do
+    (w1, w2) <- get
+    let !w =      x - a1 * w1 - a2 * w2
+    put (w, w1)
     
-    return y_n
+    return $! (b0 * w + b1 * w1 + b2 * w2)
 
 run filt = flip (evalState . mapM filt)
